5095add9d304c7dfc08b1a340bab89bd9996e3d9
Ing. Roldan D. Vargas G <rvargas@cenditel.gob.ve>
2016-04-05 11:25:35 -0430

    se agrega archivo de configuración de base de datos

---
+++ sigesic/database_config.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+DATABASES_CONFIG = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '<NAME_DB>',
+        'USER': '<USER_DB>',
+        'PASSWORD': '<PASS_DB>',
+        'HOST': '<SERVER_DB>',
+        'PORT': '<PORT_DB>',
+    }
+}
