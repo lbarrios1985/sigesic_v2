308233fa51827e8094c852f00f27aa33d70602d9
Ing. Roldan Vargas <roldandvg@gmail.com>
2016-04-23 10:21:48 -0430

    agregado decorador para login requerido

--- base/views.py
+++ base/views.py
@@ -11,6 +11,8 @@ Copyleft (@) 2016 CENDITEL nodo Mérida - https://sigesic.cenditel.gob.ve/trac/w
 # (CENDITEL) nodo Mérida - Venezuela</a>
 # @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
 from __future__ import unicode_literals
+
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
@@ -19,5 +21,6 @@ __revision__ = ""
 __docstring__ = "DoxyGen"
 
 
+@login_required
 def inicio(request):
     return render_to_response('base.template.html', {}, context_instance=RequestContext(request))
