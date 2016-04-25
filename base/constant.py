02b89ca53152b1c13d0ffb83f896a6faafd3920a
Ing. Roldan Vargas <roldandvg@gmail.com>
2016-04-25 18:14:48 -0430

    mensajes de notificación agregados

--- base/constant.py
+++ base/constant.py
@@ -38,3 +38,22 @@ SHORT_TIPO_PERSONA = (
 
 ## PERIODO DE VERIFICACION DE LA CADUCIDAD DE LA CONTRASEÑA EN DIAS
 ACTUALIZACION_PASSWORD = 90
+
+## Mensaje a mostrar al usuario cuando el registro de datos haya sido ejecutado correctamente
+CREATE_MESSAGE = _("Los datos fueron registrados correctamente")
+
+## Mensaje a mostrar cuando los datos hayan sido actualizados correctamente
+UPDATE_MESSAGE = _("Los datos fueron actualizados correctamente")
+
+## Mensaje a mostrar cuando los datos hayan sido eliminados correctamente
+DELETE_MESSAGE = _("El registro seleccionado fue eliminado correctamente")
+
+## Mensaje a mostrar cuando el usuario solicita una nueva contraseña
+NUEVA_CLAVE_MESSAGE = _("¡La nueva contraseña fue enviada a su dirección de correo electrónico!")
+
+## Mensaje desautenticación en el sistema
+LOGOUT_SECURITY_MESSAGE = _("Por su seguridad usted a sido desautenticado del sistema, debe ingresar nuevamente.")
+
+## Mensaje de error para peticiones AJAX
+MSG_NOT_AJAX = _("No se puede procesar la petición. "
+                 "Verifique que posea las opciones javascript habilitadas e intente nuevamente.")
