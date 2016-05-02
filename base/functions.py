890be25af9ee58b9d26d79f080c79cc84834712d
Ing. Roldan D. Vargas G <rvargas@cenditel.gob.ve>
2016-05-02 13:17:35 -0400

    agregada función para el cálculo de diferencia en días entre dos fechas

--- base/functions.py
+++ base/functions.py
@@ -15,12 +15,16 @@ from __future__ import unicode_literals
 import logging
 import smtplib
 
+from datetime import datetime
+
 from django.conf import settings
 from django.core.mail import send_mail
 from django.template import Context
 from django.template.loader import get_template
 
 logger = logging.getLogger('base')
+
+date_now = datetime.now()
 
 __licence__ = "GNU Public License v2"
 __revision__ = ""
@@ -104,3 +108,23 @@ def enviar_correo(email, template, subject, vars = None):
             'correo': email, 'error': e
         }, exc_info=True)
         return False
+
+
+def calcular_diferencia_fechas(fecha_inicial, fecha_final=date_now):
+    """!
+    Función que permite calcular la diferencia de días entre dos fechas
+
+    @author Ing. Roldan Vargas (rvargas at cenditel.gob.ve)
+    @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+    @date 02-05-2016
+    @param fecha_inicial <b>{object}</b> Objeto que contiene el datetime de la fecha inicial
+    @param fecha_final <b>{object}</b> Objeto que contiene el datetime de la fecha final. Su valor por defecto,
+            si no se especifica, es la fecha actual del sistema
+    @return Devuelve la diferencia del número de días entre las dos fechas
+    """
+    date_format = "%d/%m/%Y"
+    fecha_inicial = datetime.strptime(fecha_inicial.strftime(date_format), date_format)
+    fecha_final = datetime.strptime(fecha_final.strftime(date_format), date_format)
+    diff_dates = fecha_final - fecha_inicial
+
+    return diff_dates.days
