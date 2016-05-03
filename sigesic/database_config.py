10f236f7f8f485192d81138b13818183d761cdf7
Ing. Roldan D. Vargas G <rvargas@cenditel.gob.ve>
2016-05-03 10:12:17 -0400

    se agrega atributo ATOMIC_REQUEESTS en la base de datos para establecer transacciones en todas las vistas del sistema

--- sigesic/database_config.py
+++ sigesic/database_config.py
@@ -2,10 +2,11 @@
 DATABASES_CONFIG = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sigesic_v2',
-        'USER': 'admin',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': '<DB_NAME>',
+        'USER': '<DB_USER>',
+        'PASSWORD': '<DB_PASSWORD>',
+        'HOST': '<DB_HOST>',
+        'PORT': '<DB_PORT>',
+        'ATOMIC_REQUESTS': True, # Crea transacciones en cada peticion de la vista
     }
 }
