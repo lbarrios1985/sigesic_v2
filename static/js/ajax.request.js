be6bdeec1f5b7688ba2f9b612554218e6c9c7b74
Ing. Roldan D. Vargas G <rvargas@cenditel.gob.ve>
2016-05-05 12:11:04 -0400

    se agregan funciones genericas para eliminar registros, cargar datos en un select y actualizar datos de selects anidados

--- static/js/ajax.request.js
+++ static/js/ajax.request.js
@@ -90,3 +90,106 @@ function validar_rif_seniat(campo_rif, campo_nombre, registro, agente_retencion,
     }
 
 }
+
+/**
+ * @brief Función que elimina registros del sistema
+ * @param url Dirección URL de la función que atiende la petición para la eliminación de registros
+ * @param label Etiqueta de la aplicación del modelo a eliminar
+ * @param modelo Modelo en el cual se eliminarán datos
+ * @param id Identificador del registro a eliminar
+ */
+function eliminar_registro(url, label, modelo, id) {
+    bootbox.confirm(MSG_ELIMINAR_REGISTRO, function(result) {
+        if (result) {
+            $.getJSON(url, {app_label: label, modelo: modelo, id: id}, function (datos) {
+                var msg = "";
+                if (datos.resultado) {
+                    msg = MGS_REGISTRO_ELIMINADO;
+                }
+                else {
+                    msg = MSG_REGISTRO_NO_ELIMINADO;
+                }
+                bootbox.alert(msg, function() {
+                    console.log(msg);
+                    location.reload();
+                });
+            }).fail(function(jqxhr, textStatus, error) {
+                var err = textStatus + ", " + error;
+                bootbox.alert( MSG_PETICION_AJAX_FALLIDA + err );
+                console.log(MSG_PETICION_AJAX_FALLIDA + err)
+            });
+        }
+    });
+}
+
+/**
+ * @brief Función que actualiza los datos de combos dependientes
+ * @param opcion Código del elemento seleccionado por el cual se filtrarán los datos en el combo dependiente
+ * @param app Nombre de la aplicación en la cual buscar la información a filtrar
+ * @param mod Modelo del cual se van a extraer los datos filtrados según la selección
+ * @param campo Nombre del campo con el cual realizar el filtro de los datos
+ * @param n_value Nombre del campo que contendra el valor de cada opción en el combo
+ * @param n_text Nombre del campo que contendrá el texto en cada opción del combo
+ * @param combo_destino Identificador del combo en el cual se van a mostrar los datos filtrados
+ * @param bd Nombre de la base de datos, si no se específica se asigna el valor por defecto
+ */
+function actualizar_combo(opcion, app, mod, campo, n_value, n_text, combo_destino, bd) {
+    /* Verifica si el parámetro esta definido, en caso contrario establece el valor por defecto */
+    bd = typeof bd !== 'undefined' ? bd : 'default';
+    $.ajaxSetup({
+        async: false
+    });
+    $.getJSON(URL_ACTUALIZAR_SELECT, {
+        opcion:opcion, app:app, mod:mod, campo:campo, n_value:n_value, n_text: n_text, bd:bd
+    }, function(datos) {
+
+        var combo = $("#"+combo_destino);
+
+        if (datos.resultado) {
+
+            if (datos.combo_disabled == "false") {
+                combo.removeAttr("disabled");
+            }
+            else {
+                combo.attr("disabled", "true");
+            }
+
+            combo.html(datos.combo_html);
+        }
+        else {
+            bootbox.alert(datos.error);
+            console.log(datos.error);
+        }
+    }).fail(function(jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error;
+        bootbox.alert( MSG_PETICION_AJAX_FALLIDA + err );
+        console.log(MSG_PETICION_AJAX_FALLIDA + err)
+    });
+}
+
+/**
+ *
+ * @param app Nombre de la aplicación en la cual buscar la información a filtrar
+ * @param mod Modelo del cual se van a extraer los datos filtrados según la selección
+ * @param n_value Nombre del campo que contendra el valor de cada opción en el combo
+ * @param n_text Nombre del campo que contendrá el texto en cada opción del combo
+ * @param id_combo Identificador del combo en el cual se van a mostrar los datos filtrados
+ * @param bd Nombre de la base de datos, si no se específica se asigna el valor por defecto
+ */
+function cargar_combo(app, mod, n_value, n_text, id_combo, bd) {
+    bd = typeof bd !== 'undefined' ? bd : 'default';
+    $.getJSON(URL_CARGAR_COMBO, {app:app, mod:mod, n_value:n_value, n_text:n_text, bd:bd}, function(datos) {
+        var combo = $("#"+id_combo);
+        if (datos.resultado) {
+            combo.html(datos.combo_html);
+        }
+        else {
+            bootbox.alert(datos.error);
+            console.log(datos.error);
+        }
+    }).fail(function(jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error;
+        bootbox.alert( MSG_PETICION_AJAX_FALLIDA + err );
+        console.log(MSG_PETICION_AJAX_FALLIDA + err)
+    });
+}
