4f78dcc2d9b263419b162b74d9cdb4e59271000f
Ing. Roldan Vargas <roldandvg@gmail.com>
2016-05-06 11:54:23 -0430

    se aumenta el tamaño del input para registrar el número de teléfono y se modifica el placeholder

--- usuario/forms.py
+++ usuario/forms.py
@@ -186,8 +186,8 @@ class RegistroForm(ModelForm):
         max_length=20,
         widget=TextInput(
             attrs={
-                'class': 'form-control input-sm', 'placeholder': '(058)-___-_______',
-                'data-rule-required': 'true', 'data-toggle': 'tooltip', 'size': '12',
+                'class': 'form-control input-sm', 'placeholder': '(058)-000-0000000',
+                'data-rule-required': 'true', 'data-toggle': 'tooltip', 'size': '15',
                 'title': _("Indique el número telefónico de contacto con el usuario"), 'data-mask': '(000)-000-0000000'
             }
         ),
