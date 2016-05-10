d660162916a8c6e9976020ad64204835315bf063
Ing. Roldan Vargas <roldandvg@gmail.com>
2016-05-10 16:25:08 -0400

    creación de modelos para la gestión de países, estados, municipios, parroquias y ciudades

--- base/models.py
+++ base/models.py
@@ -44,3 +44,120 @@ class Notificacion(models.Model):
 
     ## Establece la relación con la Unidad Económica a la cual se le envío la notificación
     unidad_economica = models.ForeignKey(UnidadEconomica)
+
+
+@python_2_unicode_compatible
+class Pais(models.Model):
+    """!
+    Clase que contiene los países
+
+    @author Ing. Roldan Vargas (rvargas at cenditel.gob.ve)
+    @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+    @date 10-05-2016
+    @version 2.0.0
+    """
+
+    ## Nombre del pais
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s" % self.nombre
+
+    def __unicode__(self):
+        return "%s" % self.nombre
+
+
+@python_2_unicode_compatible
+class Estado(models.Model):
+    """!
+    Clase que contiene los Estados de un Pais
+
+    @author Ing. Roldan Vargas (rvargas at cenditel.gob.ve)
+    @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+    @date 10-05-2016
+    @version 2.0.0
+    """
+
+    ## Nombre del Estado
+    nombre = models.CharField(max_length=50)
+
+    ## Pais en donde esta ubicado el Estado
+    pais = models.ForeignKey(Pais)
+
+    def __str__(self):
+        return "%s" % self.nombre
+
+    def __unicode__(self):
+        return "%s" % self.nombre
+
+
+@python_2_unicode_compatible
+class Municipio(models.Model):
+    """!
+    Clase que contiene los Municipios de un Estado
+
+    @author Ing. Roldan Vargas (rvargas at cenditel.gob.ve)
+    @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+    @date 10-05-2016
+    @version 2.0.0
+    """
+
+    ## Nombre del Municipio
+    nombre = models.CharField(max_length=50)
+
+    ## Estado en donde se encuentra el Municipio
+    estado = models.ForeignKey(Estado)
+
+    def __str__(self):
+        return "%s" % self.nombre
+
+    def __unicode__(self):
+        return "%s" % self.nombre
+
+
+@python_2_unicode_compatible
+class Parroquia(models.Model):
+    """!
+    Clase que contiene las parroquias de un Municipio
+
+    @author Ing. Roldan Vargas (rvargas at cenditel.gob.ve)
+    @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+    @date 10-05-2016
+    @version 2.0.0
+    """
+
+    ## Nombre de la Parroquia
+    nombre = models.CharField(max_length=50)
+
+    ## Municipio en el que se encuentra ubicada la Parroquia
+    municipio = models.ForeignKey(Municipio)
+
+    def __str__(self):
+        return "%s" % self.nombre
+
+    def __unicode__(self):
+        return "%s" % self.nombre
+
+
+@python_2_unicode_compatible
+class Ciudad(models.Model):
+    """!
+    Clase que contiene las Ciudades de un Estado
+
+    @author Ing. Roldan Vargas (rvargas at cenditel.gob.ve)
+    @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+    @date 10-05-2016
+    @version 2.0.0
+    """
+
+    ## Nombre de la Ciudad
+    nombre = models.CharField(max_length=50)
+
+    ## Estado en donde se encuentra ubicada la Ciudad
+    estado = models.ForeignKey(Estado)
+
+    def __str__(self):
+        return "%s" % self.nombre
+
+    def __unicode__(self):
+        return "%s" % self.nombre
