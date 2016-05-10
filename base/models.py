68cd2a0b7d4a84ec84ca25e0dc94606a9ab6d603
Ing. Roldan Vargas <roldandvg@gmail.com>
2016-05-10 16:12:58 -0400

    creación de modelo para los mensajes de notificación

--- base/models.py
+++ base/models.py
@@ -17,10 +17,30 @@ from django.utils import six
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
-from base.constant import TURNO
+from base.constant import TURNO, ESTATUS_NOTIFICACION, ESTATUS_NOTIFICACION_DEFAULT
+from unidad_economica.models import UnidadEconomica
 
 __licence__ = "GNU Public License v2"
 __revision__ = ""
 __docstring__ = "DoxyGen"
 
 
+@python_2_unicode_compatible
+class Notificacion(models.Model):
+    """!
+    Clase que gestiona las notificaciones realizadas a los usuarios
+
+    @author Ing. Roldan Vargas (rvargas at cenditel.gob.ve)
+    @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+    @date 10-05-2016
+    @version 2.0.0
+    """
+
+    ## Registra el mensaje de la notificación a enviar
+    mensaje = models.TextField(help_text=_("Mensaje"))
+
+    ## Estatus del mensaje
+    estatus = models.CharField(max_length=1, choices=ESTATUS_NOTIFICACION, default=ESTATUS_NOTIFICACION_DEFAULT)
+
+    ## Establece la relación con la Unidad Económica a la cual se le envío la notificación
+    unidad_economica = models.ForeignKey(UnidadEconomica)
