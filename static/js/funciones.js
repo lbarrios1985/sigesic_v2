55da6bbf7b30b95e0d0f2c7dd68ca4813345f9f8
Ing. Roldan D. Vargas G <rvargas@cenditel.gob.ve>
2016-05-17 13:23:21 -0400

    función para seleccionar coordenadas geográficas

--- static/js/funciones.js
+++ static/js/funciones.js
@@ -72,3 +72,96 @@ function anho_registro(title, template) {
     }
     $(modal).find('.select2').select2({});
 }
+
+/**
+ * @brief Función que muestra una ventana emergente con un mapa para la selección de las coordenadas geográficas
+ * @param title Titulo de la ventana emergente
+ * @param template Plantilla html a utilizar para mostrar el formulario
+ */
+function seleccionar_coordenadas(title, template) {
+    var modal = bootbox.dialog({
+        title: title,
+        message: template,
+        buttons: {
+            success: {
+                label: BTN_REGISTRAR,
+                className: "btn btn-primary btn-sm",
+                callback: function() {
+                    //Registro de las coordenadas o actualizacion de coordenadas
+                }
+            },
+            main: {
+                label: BTN_CANCELAR,
+                className: "btn btn-warning btn-sm"
+            }
+        }
+    });
+
+    $(document).ready(function() {
+        var satellite = new ol.layer.Tile({
+            source: new ol.source.MapQuest({layer: 'sat'})
+        });
+
+        var osm = new ol.layer.Tile({
+            source: new ol.source.MapQuest({layer: 'osm'})
+        });
+
+        satellite.on('tileloadstart', function() {
+            cargando.showPleaseWait();
+        });
+
+        satellite.on('tileloadend', function() {
+            cargando.hidePleaseWait();
+        });
+
+        var map = new ol.Map({
+            target: 'map',
+            layers: [satellite, osm],
+            view: new ol.View({
+                center: ol.proj.transform([-65.0000,6.5000], 'EPSG:4326', 'EPSG:900913'),
+                zoom: 4
+            })
+        });
+
+        var mousePosition = new ol.control.MousePosition({
+            coordinateFormat: ol.coordinate.createStringXY(6),
+            projection: 'EPSG:4326',
+            target: document.getElementById('myposition'),
+            undefinedHTML: '&nbsp;'
+        });
+
+        map.addControl(mousePosition);
+
+        map.on("click", function(evt) {
+
+            // agregar funcionalidad para el marcador
+
+            /*iconFeatures = [];
+            var iconFeature = new ol.Feature({
+              geometry: new ol.geom.Point(ol.proj.transform(evt.coordinate, 'EPSG:4326',
+              'EPSG:900913'))
+            });
+
+            iconFeatures.push(iconFeature);
+
+            var vectorSource = new ol.source.Vector({
+              features: iconFeatures //add an array of features
+            });
+
+            var iconStyle = new ol.style.Style({
+              image: new ol.style.Icon(({
+                anchor: [0.5, 46],
+                anchorXUnits: 'fraction',
+                anchorYUnits: 'pixels'
+              }))
+            });
+
+
+            var vectorLayer = new ol.layer.Vector({
+              source: vectorSource,
+              style: iconStyle
+            });*/
+        });
+    });
+
+}
