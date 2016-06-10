218ab2eafbe53ffd407211330787d8e8477ebdc7
eramirez <eramirez@cenditel.gob.ve>
2016-06-10 15:50:07 -0400

    Datos iniciales

--- sigesic/database_config.py
+++ sigesic/database_config.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 DATABASES_CONFIG = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sigesic_v2',
-        'USER': 'postgres',
-        'PASSWORD': 'admin8080',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'ATOMIC_REQUESTS': True, # Crea transacciones en cada peticion de la vista
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': '<DB_NAME>',
+       'USER': '<DB_USER>',
+       'PASSWORD': '<DB_PASSWORD>',
+       'HOST': 'DB_HOST',
+       'PORT': 'DB_PORT',
+      'ATOMIC_REQUESTS': True, # Crea transacciones en cada petición de la vista
+  }
 }
