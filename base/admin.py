19b3ae832b852fa9c6fe68d4d993a4e9563e0ad0
Ing. Roldan D. Vargas G <rvargas@cenditel.gob.ve>
2016-06-23 13:52:46 -0400

    agregada clase para la gestión de años de registro en el panel administrativo

--- base/admin.py
+++ base/admin.py
@@ -14,6 +14,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import AnhoRegistro
+
 import logging
 
 logger = logging.getLogger("base")
@@ -23,3 +25,20 @@ __revision__ = ""
 __docstring__ = "DoxyGen"
 
 
+class AnhoRegistroAdmin(admin.ModelAdmin):
+    """!
+    Clase que gestiona los años de registro en el panel administrativo
+
+    @author Ing. Roldan Vargas (rvargas at cenditel.gob.ve)
+    @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+    @date 20-04-2016
+    @version 2.0.0
+    """
+    list_display = ('anho',)
+    list_filter = ('anho',)
+    ordering = ('anho',)
+    search_fields = ('anho',)
+
+
+## Registra el modelo AnhoRegistro en el panel administrativo
+admin.site.register(AnhoRegistro, AnhoRegistroAdmin)
