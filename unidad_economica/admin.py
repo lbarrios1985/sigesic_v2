f1b817a156ed6842029d3f35146aaf564dbefaf9
Ing. Roldan D. Vargas G <rvargas@cenditel.gob.ve>
2016-06-23 13:51:34 -0400

    agregada clase para la gestión de notificaciones en el panel administrativo

--- unidad_economica/admin.py
+++ unidad_economica/admin.py
@@ -10,8 +10,34 @@ Copyleft (@) 2016 CENDITEL nodo Mérida - https://sigesic.cenditel.gob.ve/trac/
 # @author <a href='​http://www.cenditel.gob.ve'>Centro Nacional de Desarrollo e Investigación en Tecnologías Libres (CENDITEL) nodo Mérida - Venezuela</a>
 # @copyright <a href='​http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
 from __future__ import unicode_literals
+
 from django.contrib import admin
+
+from .models import Notificacion
+
+import logging
+
+logger = logging.getLogger("ue")
 
 __licence__ = "GNU Public License v2"
 __revision__ = ""
 __docstring__ = "DoxyGen"
+
+
+class NotificacionAdmin(admin.ModelAdmin):
+    """!
+    Clase que gestiona las notificaciones en el panel administrativo
+
+    @author Ing. Roldan Vargas (rvargas at cenditel.gob.ve)
+    @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+    @date 20-04-2016
+    @version 2.0.0
+    """
+    list_display = ('unidad_economica', 'mensaje', 'estatus')
+    list_filter = ('unidad_economica', 'estatus')
+    ordering = ('unidad_economica', 'estatus')
+    search_fields = ('unidad_economica', 'estatus', 'mensaje')
+
+
+## Registra el modelo Notificacion en el panel administrativo del sistema
+admin.site.register(Notificacion, NotificacionAdmin)
