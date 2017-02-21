35a7f5b0e082df98fbed4a5da6d5a118f01fefab
Ing. Roldan D. Vargas G <rvargas@cenditel.gob.ve>
2017-02-21 12:25:26 -0400

    agregado requerimiento e instrucciones para celery

--- sigesic/celery.py
+++ sigesic/celery.py
@@ -3,15 +3,25 @@ Sistema Integral de Gestión para la Industria y el Comercio (SIGESIC)
 
 Copyleft (@) 2016 CENDITEL nodo Mérida - https://sigesic.cenditel.gob.ve/trac/wiki
 """
-## @package sigesic.__init__
+## @package sigesic.celery
 #
-# Contiene instrucciones iniciales para el módulo de configuración de la aplicación
+# Contiene la configuración para la ejecución de tareas de Celery
 # @author Ing. Roldan Vargas (rvargas at cenditel.gob.ve)
 # @author <a href='http://www.cenditel.gob.ve'>Centro Nacional de Desarrollo e Investigación en Tecnologías Libres
 # (CENDITEL) nodo Mérida - Venezuela</a>
 # @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
 from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
 
-from .celery import app as celery_app
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sigesic.settings')
 
-__all__ = ['celery_app']
+app = Celery('sigesic')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+	print('Request: {0!r}'.format(self.request))
